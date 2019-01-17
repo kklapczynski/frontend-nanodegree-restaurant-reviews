533e9bc34a172ca223511c36e64de7a83c85aa72
Krzysztof Klapczyński <k.klapczynski@gmail.com>
2019-01-17 12:21:04 +0100

    refactor: Improves accessibility.
    Moves map section to give priority of tabindex to filters on main page.
    
    Removes some links from map component from tabindex.
    
    Adds "lang" attribute to html tags.

--- js/main.js
+++ js/main.js
@@ -80,9 +80,9 @@ initMap = () => {
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
     mapboxToken: 'pk.eyJ1Ijoia2tsYXBjenluc2tpIiwiYSI6ImNqcW1hcmx3aTBheHk0OG11MDBuOWFiZzAifQ.iaDMo3GDB5VMwn1Fiv95Xg',
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data &copy; <a tabindex=-1 href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a tabindex=-1 href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a tabindex=-1 href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets'
   }).addTo(newMap);
 
